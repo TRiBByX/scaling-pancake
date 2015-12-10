27d4cebe28539e5ddfbbc2e047951053993099bb
TRiBByX <Christoffer.ssj@gmail.com>
2015-12-10 13:22:01 +0100

    UnitTest project added and preperations for Unittests has been done

---
+++ ScalingPancakeUnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using scaling_pancake;
+
+namespace ScalingPancakeUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CustomerObjectTestMethod()
+        {
+            Customer customer = new Customer("Albert", "yolo@mail.co.uk", "1234");
+        }
+
+        [TestMethod]
+        public void InputCheckerTestMethod()
+        {
+            InputChecker.NullChecker("");
+        }
+
+        [TestMethod]
+        public void Whatevertestmethod()
+        {
+            try
+            {
+                Customer albert = new Customer(123, 123, 123);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.Fail("some shit happened");
+            }
+        }
+
+    }
+}
